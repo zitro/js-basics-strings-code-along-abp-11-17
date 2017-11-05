d3622835eb0b2d30e10822bd7fdd84f0ee1b89c2
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-05 20:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 let currentUser = 'Archer';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase + '!';
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
