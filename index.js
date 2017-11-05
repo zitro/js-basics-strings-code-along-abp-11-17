908ac16d0fafd1f6d1a929e0e1a6ae6a0ca5d237
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-05 20:03:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+
+
+let currentUser = 'Archer';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
